515b2dccc79ec6a7d2dfc57b3d47d58b9b16d82e
Odilbek <=>
2026-04-18 15:46:28 +0500

    wedwed

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,6 +1,8 @@
 from aiogram import Router
 
 from handlers.users.search import router as search_router
+from handlers.users.start import router as start_router
 
 router = Router()
 router.include_router(search_router)
+router.include_router(start_router)
